cb81d8dd04d6e040cda328414228997a7fdcc6c1
unknown <elijah.smith23@yahoo.com>
2019-09-17 12:40:17 -0400

    Assignment and Comparison Operators

---
+++ 5-expressionsAndOperators/01-assignmentOperators.js
@@ -0,0 +1,17 @@
+// ASSIGNMENT
+let r = 5; // r = 5
+
+// ADDITION
+ r += 1; // r = r + 1
+
+ //SUBTRACTION
+ r -= 1 // r = r -1
+
+ //DIVISION
+ r /= 1 // r = r / 1
+
+ //REMAINDER OR MODULUS
+ r %= 1 // r = r % 1
+
+ //EXPONENTIAL
+ r **= 1 // r = r ** 1
